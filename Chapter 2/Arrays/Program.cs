61a972274f231f839ce4ec47417d2b970e23e3bd
Jose M. Puebla <joey.manuel132@gmail.com>
2024-11-24 20:37:32 -0700

    Fifth Push
    Chapter 2 Arrays, Type_Conversion

---
+++ Chapter 2/Arrays/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public void One_Dimensional_Arrays()
+        {
+            int[] arr1;
+            int[] arr2 = null;
+            int[] arr3 = new int[6];
+            int[] arr4 = new int[] { 1, 1, 2, 3, 5, 8 };
+            int[] arr5 = new int[6] { 1, 1, 2, 3, 5, 8 };
+            int[] arr6 = { 1, 1, 2, 3, 5, 8 };
+
+            // Second Example
+            for (int i = 0; i < arr6.Length; i++)
+                Console.WriteLine(arr6[i]);
+
+            foreach (int element in arr6)
+                Console.WriteLine(element);
+
+            // Third example
+            for (int i = 0; i < arr6.Length; ++i)
+                arr6[i] *= 2;  // OK
+            foreach (int element in arr6)
+                element *= 2;  // error
+        }
+
+        public void Multi_Dimensional_Array()
+        {
+            // First Example 
+            int[,] arr1;
+            arr1 = new int[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } };
+            int[,] arr2 = null;
+            int[,] arr3 = new int[2, 3];
+            int[,] arr4 = new int[,] { { 1, 2, 3 }, { 4, 5, 6 } };
+            int[,] arr5 = new int[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } };
+            int[,] arr6 = { { 1, 2, 3 }, { 4, 5, 6 } };
+
+            // Second Example
+            for (int i = 0; i < arr6.GetLength(0); ++i)
+            {
+                for (int j = 0; j < arr6.GetLength(1); ++j)
+                {
+                    Console.Write($"{arr6[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+
+            // Third Example
+            int[,,] arr7 = new int[4, 3, 2]
+                {
+                    { { 11, 12}, { 13, 14}, {15, 16 } },
+                    { { 21, 22}, { 23, 24}, {25, 26 } },
+                    { { 31, 32}, { 33, 34}, {35, 36 } },
+                    { { 41, 42}, { 43, 44}, {45, 46 } }
+                };
+        }
+
+        public void Jagged_Array()
+        {
+            int[][] arr1;
+            int[][] arr2 = null;
+            int[][] arr3 = new int[2][];
+            arr3[0] = new int[3];
+            arr3[1] = new int[] { 1, 1, 2, 3, 5, 8 };
+            int[][] arr4 = new int[][]
+            {
+new int[] { 1, 2, 3 },
+new int[] { 1, 1, 2, 3, 5, 8 }
+            };
+            int[][] arr5 =
+            {
+new int[] { 1, 2, 3 },
+new int[] { 1, 1, 2, 3, 5, 8 }
+};
+            int[][,] arr6 = new int[][,]
+            {
+new int[,] { { 1, 2}, { 3, 4 } },
+new int[,] { {11, 12, 13}, { 14, 15, 16} }
+            };
+
+            // Second example
+            for (int i = 0; i < arr5.Length; ++i)
+            {
+                for (int j = 0; j < arr5[i].Length; ++j)
+                {
+                    Console.Write($"{arr5[i][j]} ");
+                }
+                Console.WriteLine();
+            }
+        } // Jagged_Array method
+    }
+}
---
+++ Chapter 2/Type_Conversion/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Type_Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        } // Main Method
+
+
+        public void Implicit_Type_Conversion()
+        {
+            // Frist Example
+            int i = 10;
+            float f = i;
+            long l = 7195467872;
+            double d = l;
+
+            // Second example
+            string s = "example";
+            object o = s;
+        } // Implicit_Type_Conversion method
+
+
+        public void Explicit_Type_Conversion()
+        {
+            // First Example
+
+            double d = 12.34;
+            int i = (int)d;
+
+            // Second Example
+
+            string s = "example";
+            object o = s;          // implicit conversion
+            string r = (string)o;  // explicit conversion
+        } // Explicit_Type_Conversion method
+
+        public void User_Defined_Type_Conversions()
+        {
+            // First Example
+            /* public readonly struct fancyint
+{
+    private readonly int value;
+    public fancyint(int value)
+    {
+        this.value = value;
+    }
+    public static implicit operator int(fancyint v) => v.value;
+    public static explicit operator fancyint(int v) => new fancyint(v);
+    public override string ToString() => $"{value}";
+}*/
+
+            // Second Example
+            /*fancyint a = new fancyint(42);
+int i = a;                 // implicit conversion
+fancyint b = (fancyint)i;  // explicit conversion
+             * 
+             */
+
+            // Third Exmaple
+            DateTime dt1 = DateTime.Parse("2019.08.31");
+            DateTime.TryParse("2019.08.31", out DateTime dt2);
+            int i1 = int.Parse("42");          // successful, i1 = 42
+            int i2 = int.Parse("42.15");       // error, throws exception
+            int.TryParse("42.15", out int i3); // error, returns false, 
+                                               // i3 = 0
+        } // User_Defined_Type_Conversion
+    }
+
+}
+ 
